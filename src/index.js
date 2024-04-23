e8820482ab9c02f8069d959cd6783d4497a8714b
akshat <akshatbhatya@gmail.com>
2024-04-23 23:04:51 +0530

    config half index file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
+import connectDb from "./db/index.js";
+import app from "./app.js";
+connectDb().then(()=>{
+    app.listen(()=>{
+        console.log(`${}`);
+    })
+
+})
