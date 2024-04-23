802d49652f8a7a774ff17b81f93ab9410c01a5b6
akshat <akshatbhatya@gmail.com>
2024-04-23 22:50:49 +0530

    add some middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
 import express from "express";
-
-const app=express();
+import cors from "cors";
+const app = express();
 
 app.use(express.static("public"));
+
+app.use(
+  express.urlencoded({
+    limit: "16kb",
+    extended: true,
+  })
+);
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+export default app;
