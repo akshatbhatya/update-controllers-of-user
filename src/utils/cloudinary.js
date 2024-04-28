9ef59ff90bd3422cde9dc6efb4e98c0aa615337c
akshat <akshatbhatya@gmail.com>
2024-04-28 13:07:45 +0530

    add cloudinary features file uploader

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.cloudinary_cloud_name,
+  api_key: process.env.cloudinary_api_key,
+  api_secret: process.env.cloudinary_api_secret,
+});
+
+async function uploadLocalFile(localFile) {
+  try {
+    await cloudinary.uploader.upload(localFile, { resource_type: "auto" });
+    fs.unlinkSync(localFile);
+  } catch (error) {
+    fs.unlinkSync(localFile);
+  }
+}
+
+export default uploadLocalFile;
