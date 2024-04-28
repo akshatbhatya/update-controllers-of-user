3cb22512a13bfd4b146ec5c21e377bad0ef09e1b
akshat <akshatbhatya@gmail.com>
2024-04-28 18:22:00 +0530

    response api is done

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,5 +1,10 @@
 class ApiResponse {
-  constructor() {}
+  constructor(status, message, data) {
+    (this.status = status),
+      (this.message = message),
+      (this.data = data),
+      (this.succees = status < 400);
+  }
 }
 
 export default ApiResponse;
